cb3777c54309db784d4da2f13aad7651a1da466e
dberdasco99 <d.berdasco.gonzalez@accenture.com>
2025-11-12 13:48:36 +0100

    Update run_semgrep.sh

--- preflight/run_semgrep.sh
+++ preflight/run_semgrep.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -e
-
 echo "Haciendo Semgrep SAST..."
-
-# Crear y activar virtualenv
-python3 -m venv .venv
-source .venv/bin/activate
-
-# Actualizar pip e instalar semgrep
-pip install --upgrade pip
-pip install semgrep
-
-# Ejecutar semgrep
 semgrep --config auto --json --output reports/${APP_NAME}-semgrep.json || true
-
-# Desactivar virtualenv
-deactivate
